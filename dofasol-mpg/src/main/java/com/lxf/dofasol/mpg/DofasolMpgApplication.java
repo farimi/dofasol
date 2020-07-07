c6497e07632e1e6af1465039ff35802586b19c5d
1343106657@qq.com <1343106657@qq.com>
2020-07-07 13:18:01 +0800

    create mpg

---
+++ dofasol-mpg/src/main/java/com/lxf/dofasol/mpg/DofasolMpgApplication.java
@@ -0,0 +1,13 @@
+package com.lxf.dofasol.mpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofasolMpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DofasolMpgApplication.class, args);
+    }
+
+}
---
+++ dofasol-mpg/src/test/java/com/lxf/dofasol/mpg/DofasolMpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxf.dofasol.mpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofasolMpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
